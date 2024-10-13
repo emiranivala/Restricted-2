1452b555fc311ae0d7f561894a6bf6beb91dafda
emiranivala <taitufam@gmail.com>
2024-10-13 07:41:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "22836334"))
+API_HASH = getenv("API_HASH", "072b12319aa045abd8710bd92eafbf4b")
+BOT_TOKEN = getenv("BOT_TOKEN", "7617250222:AAH6Hwu0ST0MAQItKWRVr12Gxx6o1zhM1cM")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7308378079").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://abayedrisemmons:zh0qKvRKtatgdSmV@cluster0.ct8gs.mongodb.net/?retryWrites=true&w=majority")
+LOG_GROUP = getenv("LOG_GROUP", "1002265762140")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002265762140"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002265762140"))
 DEFAULT_SESSION = getenv("DEFAULT_SESSION", "")
